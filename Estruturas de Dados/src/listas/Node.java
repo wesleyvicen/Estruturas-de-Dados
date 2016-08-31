d730f35bfbd0fae2a450b0e818b9f49bd225ffce
aldo <aldo@aldo-Inspiron-3443>
2016-08-31 16:53:39 -0300

    Node

--- Estruturas de Dados/src/listas/Node.java
+++ Estruturas de Dados/src/listas/Node.java
@@ -1,5 +1,37 @@
 package listas;
 
-public class Node {
+public class Node<T> {
+
+	private T info;
+
+	public T getInfo() {
+		return info;
+	}
+
+	public void setInfo(T info) {
+		this.info = info;
+	}
+
+	public Node<T> getNext() {
+		return next;
+	}
+
+	public void setNext(Node<T> next) {
+		this.next = next;
+	}
+
+	private Node<T> next;
+
+	public Node(T elemento) {
+		info = elemento;
+	}
+
+	public Node(T elemento, Node<T> Proximo) {
+		info = elemento;
+		next = Proximo;
+	}
+
+	public Node() {
+	}
 
 }
